a635351ed6403f8d38fa147b75221745d9054429
Amudhayazhini <yazhiniamudha2007@gmail.com>
2026-05-12 17:47:03 +0530

    Create quize.cpp

---
+++ quize.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // ADMIN ACCOUNT
+    string adminID;
+    string adminPassword;
+
+    string questions[5];
+    string options[5][4];
+    int correctAnswers[5];
+    int studentAnswers[5];
+    string studentNames[10];
+int rollNumbers[10];
+string studentPasswords[10];
+int studentcount=0;
+int choice;
+cout<<"==================================\n";
+cout<<"
